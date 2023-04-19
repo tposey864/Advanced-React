7de4723e8da88edc116061b885cf0d0f99225d65
unknown <teeposey@gmail.com>
2023-04-19 00:59:17 -0400

    completed video 8

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,10 +1,10 @@
-import Document, { Html, NextScript, Main } from 'next/document';
+import Document, { Head, Html, NextScript, Main } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en-US">
-        {/* <Head></Head> */}
+        <Head />
         <body>
           <Main />
           <NextScript />
